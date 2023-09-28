273badd57c71d1433b75ee79e5af3e7d1f050451
kinest22 <nirvana897@gmail.com>
2023-09-28 14:59:40 +0300

    CouponPeriod updated

--- src/Extensions/CouponPeriod.cs
+++ src/Extensions/CouponPeriod.cs
@@ -9,15 +9,14 @@ namespace RuDataAPI.Extensions
     {
         public CouponPeriod(CouponsFields fields)
         {
-            CouponId = fields.id_coupon;
-            CouponStart = fields.begin_period;
-            CouponEnd = fields.end_period;
-            CouponLength = fields.coupon_period;
-            CouponRate = (double?)fields.coupon_rate;
-            Spread = (double?)fields.rate_spread_pct;
-            CouponPayment = (double?)fields.pay_per_bond;
+            CouponId            = fields.id_coupon;
+            CouponStart         = fields.begin_period!.Value;
+            CouponEnd           = fields.end_period!.Value;
+            CouponLength        = fields.coupon_period!.Value;
+            CouponRate          = (double?)fields.coupon_rate ?? default;
+            Spread              = (double?)fields.rate_spread_pct ?? default;
+            CouponPayment       = (double?)fields.pay_per_bond ?? default;
         }
-
 
         /// <summary>
         ///     Coupon ID.
@@ -27,31 +26,31 @@ namespace RuDataAPI.Extensions
         /// <summary>
         ///     Start date of coupon period.
         /// </summary>
-        public DateTime? CouponStart { get; set; }   
+        public DateTime CouponStart { get; set; }   
         
         /// <summary>
         ///     End date of coupon period.
         /// </summary>
-        public DateTime? CouponEnd { get; set; }
+        public DateTime CouponEnd { get; set; }
         
         /// <summary>
         ///     Length of coupon period in days.
         /// </summary>        
-        public long? CouponLength { get; set; }
+        public long CouponLength { get; set; }
 
         /// <summary>
         ///     Annual interest rate established for the coupon period.
         /// </summary>
-        public double? CouponRate { get; set; }
+        public double CouponRate { get; set; }
 
         /// <summary>
         ///     Difference between Coupon rate and reference floating rate.
         /// </summary>
-        public double? Spread { get; set; }
+        public double Spread { get; set; }
 
         /// <summary>
         ///     Payment per bond for this coupon period in units of bond's notional currency.
         /// </summary>
-        public double? CouponPayment { get; set; }
+        public double CouponPayment { get; set; }
     }
 }

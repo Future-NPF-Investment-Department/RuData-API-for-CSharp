68ae23255eba07d29a69b0cb016cfb9765b3e7e2
kinest22 <nirvana897@gmail.com>
2023-10-03 18:06:36 +0300

    CreditRating class added

---
+++ src/Extensions/CreditRating.cs
@@ -0,0 +1,31 @@
+﻿#pragma warning disable CS0660, CS0661 // Type defines operator == or operator != but does not override Object.Equals(object o)
+
+using RuDataAPI.Extensions.Mapping;
+
+namespace RuDataAPI.Extensions
+{
+    public class CreditRating
+    {
+        public string Value { get; set; } = null!;
+        public CreditRatingValue ValueAggregated { get; set; }
+        public CreditRatingScale Scale { get; set; }
+        public CreditRatingCurrency Currency { get; set; }
+        public DateTime Date { get; set; }
+        public string Agency { get; set; } = null!;
+        public string Action { get; set; } = null!;
+        public double DefaultProbability { get; set; } 
+
+
+        public static bool operator <=(CreditRating rating1, CreditRatingValue value)
+            => rating1.ValueAggregated <= value;    
+
+        public static bool operator >=(CreditRating rating1, CreditRatingValue value)
+            => rating1.ValueAggregated >= value;
+
+        public static bool operator ==(CreditRating rating1, CreditRatingValue value)
+            => rating1.ValueAggregated == value;
+
+        public static bool operator !=(CreditRating rating1, CreditRatingValue value)
+            => rating1.ValueAggregated != value;
+    }
+}

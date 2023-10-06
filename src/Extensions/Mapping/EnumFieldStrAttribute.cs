d243d7328692f69322b65892ca94f8bc6f811503
kinest22 <nirvana897@gmail.com>
2023-10-06 15:04:21 +0300

    EnumFieldStrAttribute added

---
+++ src/Extensions/Mapping/EnumFieldStrAttribute.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace RuDataAPI.Extensions.Mapping
+{
+    /// <summary>
+    ///     Represents alternative string description for enum field.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public class EnumFieldStrAttribute : Attribute
+    {
+        public EnumFieldStrAttribute(string stringVal) 
+            => Value = stringVal;
+        
+        public string Value { get; set; }
+    }
+}

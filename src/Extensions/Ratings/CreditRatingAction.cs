b42179755d4c76bf330e3480a9fc577e2f1699c6
kinest22 <nirvana897@gmail.com>
2023-10-06 17:00:41 +0300

    CreditRatingAction and CreditRatingOutlook enums added

---
+++ src/Extensions/Ratings/CreditRatingAction.cs
@@ -0,0 +1,25 @@
+﻿using RuDataAPI.Extensions.Mapping;
+
+namespace RuDataAPI.Extensions.Ratings
+{
+    /// <summary>
+    ///     Represents a rating action carried out by a rating agency in respect of emitent or security credit rating.
+    /// </summary>
+    public enum CreditRatingAction
+    {
+        [EnumFieldStr("Установлен")]
+        Assigned,
+
+        [EnumFieldStr("Повышен")]
+        Upgraded,
+
+        [EnumFieldStr("Понижен")]
+        Downgraded,
+
+        [EnumFieldStr("Подтвержден")]
+        Affirmed,
+
+        [EnumFieldStr("снят")]
+        Withdrawed
+    }
+}
---
+++ src/Extensions/Ratings/CreditRatingOutlook.cs
@@ -0,0 +1,25 @@
+﻿using RuDataAPI.Extensions.Mapping;
+
+namespace RuDataAPI.Extensions.Ratings
+{
+    /// <summary>
+    ///     Represents rating oultook (forecast) of emitent or security.
+    /// </summary>
+    public enum CreditRatingOutlook
+    {
+        [EnumFieldStr("Позитивный")]
+        Positive,
+
+        [EnumFieldStr("Негативный")]
+        Negative,
+
+        [EnumFieldStr("Стабильный")]
+        Stable,
+
+        [EnumFieldStr("Неопределенный")]
+        Uncertain,
+
+        [EnumFieldStr("на пересмотре")]
+        Revising
+    }
+}

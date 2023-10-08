b1a831ecbd6aadf5bcdb5e9c02000e56296d7f4a
kinest22 <nirvana897@gmail.com>
2023-10-08 03:05:47 +0300

    Credit rating target enum added

---
+++ src/Extensions/Ratings/CreditRatingTarget.cs
@@ -0,0 +1,19 @@
+﻿
+namespace RuDataAPI.Extensions.Ratings
+{
+    /// <summary>
+    ///     Represents object to which credit rating action applied.
+    /// </summary>
+    public enum CreditRatingTarget
+    {
+        /// <summary>
+        ///     Credit rating action applied to issuer.
+        /// </summary>
+        Issuer,
+
+        /// <summary>
+        ///     Credit rating action applied to issuer's security.
+        /// </summary>
+        Security
+    }
+}

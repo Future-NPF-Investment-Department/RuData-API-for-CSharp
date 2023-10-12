b74fe519e1adafc63a6c94a5c34d61d9f33aa35b
kinest22 <nirvana897@gmail.com>
2023-10-12 18:09:03 +0300

    aggregated credit rating class added

---
+++ src/Extensions/Ratings/CreditRatingAggregated.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace RuDataAPI.Extensions.Ratings
+{
+    public class CreditRatingAggregated
+    {
+        public CreditRatingUS RatingBig3 { get; set; }
+        public CreditRatingRU RatingRu { get; set; }
+        public CreditRatingScale Scale { get; set; }
+        public CreditRatingCurrency Currency { get; set; }
+        public IReadOnlyList<CreditRating> Ratings { get; set; }
+        
+    }
+}

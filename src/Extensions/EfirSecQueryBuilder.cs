4d69b49006099d3a4630ccb3dc52fc01abdd6316
kinest22 <nirvana897@gmail.com>
2023-10-16 02:40:39 +0300

    EfirSecQueryBuilder added

---
+++ src/Extensions/EfirSecQueryBuilder.cs
@@ -0,0 +1,69 @@
+﻿
+using RuDataAPI.Extensions.Ratings;
+
+namespace RuDataAPI.Extensions
+{
+    public sealed class EfirSecQueryBuilder
+    {
+        private readonly EfirSecQueryDetails _query;
+        public EfirSecQueryBuilder()
+            => _query = new EfirSecQueryDetails();
+
+
+        public EfirSecQueryBuilder WithDistributionDateStart(DateTime start)
+        {
+            _query.DistributionStart = start;
+            return this;
+        }
+
+        public EfirSecQueryBuilder WithDistributionDateEnd(DateTime end)
+        {
+            _query.DistributionEnd = end;
+            return this;
+        }
+
+        public EfirSecQueryBuilder WithMaturityDateStart(DateTime start)
+        {
+            _query.MaturityStart = start;
+            return this;
+        }
+
+        public EfirSecQueryBuilder WithMaturityDateEnd(DateTime end)
+        {
+            _query.MaturityEnd = end;
+            return this;
+        }
+
+        public EfirSecQueryBuilder WithLowRating(CreditRatingUS rating)
+        {
+            _query.RatingLow = rating;
+            return this;
+        }
+
+        public EfirSecQueryBuilder WithHighRating(CreditRatingUS rating)
+        {
+            _query.RatingHigh = rating;
+            return this;
+        }
+
+        public EfirSecQueryBuilder WithSector(params string[] sectors)
+        {
+            _query.Sectors = sectors;
+            return this;
+        }
+
+        public EfirSecQueryBuilder WithCurrency(string currecny)
+        {
+            _query.Currency = currecny;
+            return this;
+        }
+
+        public EfirSecQueryBuilder WithIssuerCountry(string countryCode)
+        {
+            _query.Country = countryCode;
+            return this;
+        }
+
+        public EfirSecQueryDetails Build() => _query;
+    }
+}
---
+++ src/Extensions/EfirSecQueryDetails.cs
@@ -0,0 +1,19 @@
+﻿
+using RuDataAPI.Extensions.Ratings;
+
+namespace RuDataAPI.Extensions
+{
+    public class EfirSecQueryDetails
+    {
+        public DateTime? DistributionStart { get; set; }
+        public DateTime? DistributionEnd { get; set; }
+        public DateTime? MaturityStart { get; set; }
+        public DateTime? MaturityEnd { get; set; }
+        public CreditRatingUS? RatingLow { get; set; }
+        public CreditRatingUS? RatingHigh { get; set; }       
+        public string[]? Sectors { get; set; }
+        public string? Currency { get; set; }
+        public string? Country { get; set; }    
+
+    }
+}

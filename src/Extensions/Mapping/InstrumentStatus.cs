5f62f96f55de7fc6d87edfdf38ec8383bb95ebe7
kinest22 <nirvana897@gmail.com>
2023-10-30 13:16:19 +0300

    Instrument status enum added to mappings

---
+++ src/Extensions/Mapping/InstrumentStatus.cs
@@ -0,0 +1,26 @@
+﻿namespace RuDataAPI.Extensions.Mapping
+{
+    /// <summary>
+    ///     Represents the status of financial intrument / security.
+    /// </summary>
+    public enum InstrumentStatus
+    {
+        [EnumFieldStr("Не определено")]
+        Na,
+
+        [EnumFieldStr("В обращении", "Размещен")]
+        InCirculation,
+
+        [EnumFieldStr("Готовится", "Размещается")]
+        InPreparation,
+
+        [EnumFieldStr("Дефолт")]
+        Defaulted,
+
+        [EnumFieldStr("Досрочно погашен", "Погашен")]
+        Redeemed,
+
+        [EnumFieldStr("Аннулирован", "Прекращение", "Приостановлен", "Списано")]
+        Canceled,
+    }
+}

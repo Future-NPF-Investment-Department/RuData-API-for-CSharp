a9d464d93abd36efbb6f9b62a3bc3b595d31d934
kinest22 <nirvana897@gmail.com>
2023-10-30 16:36:22 +0300

    instrument flags enum added to mappings

---
+++ src/Extensions/Mapping/InstrumentFlags.cs
@@ -0,0 +1,18 @@
+﻿
+namespace RuDataAPI.Extensions.Mapping
+{
+    /// <summary>
+    ///     Security issue classifications.
+    /// </summary>
+    [Flags]
+    public enum InstrumentFlags
+    {
+        None            = 0,
+        Structured      = 1,
+        Secured         = 2,
+        Subordinated    = 4,
+        Linker          = 8,
+        Convertible     = 16,
+        Guaranteed      = 32,
+    }
+}

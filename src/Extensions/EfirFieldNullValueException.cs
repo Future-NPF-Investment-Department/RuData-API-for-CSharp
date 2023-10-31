1252feb725b120cf753fcd62edb38e3a7b5441af
kinest22 <nirvana897@gmail.com>
2023-10-31 13:19:40 +0300

    EfirFieldNullValueException class added

---
+++ src/Extensions/EfirFieldNullValueException.cs
@@ -0,0 +1,12 @@
+﻿namespace RuDataAPI.Extensions
+{
+    public sealed class EfirFieldNullValueException : NullReferenceException
+    {
+#pragma warning disable IDE1006 // Naming Styles
+        private const string MSGHEAD = "EFIR FIELD NULL VALUE ERROR: ";
+#pragma warning restore IDE1006 // Naming Styles
+        public EfirFieldNullValueException(string message) : base(MSGHEAD + message) 
+        {
+        }  
+    }
+}

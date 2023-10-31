9b6b91b7ae60fe70e3ee54887b8cda8c111bce33
kinest22 <nirvana897@gmail.com>
2023-10-31 16:22:33 +0300

    InstrumentHistoryRecord class added

---
+++ src/Extensions/InstrumentHistoryRecord.cs
@@ -0,0 +1,45 @@
+﻿namespace RuDataAPI.Extensions
+{
+    public class InstrumentHistoryRecord
+    {
+        /// <summary>
+        ///     Trade date.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        ///     Open price for particular trade date.
+        /// </summary>
+        public double Open { get; set; }    
+
+        /// <summary>
+        ///     Minimal price for particulart trade date.
+        /// </summary>
+        public double Low { get; set; }
+
+        /// <summary>
+        ///     Maximal price for particulart trade date.
+        /// </summary>
+        public double High { get; set; }
+
+        /// <summary>
+        ///     Close price for particulart trade date.
+        /// </summary>
+        public double Close { get; set; }  
+        
+        /// <summary>
+        ///     Aggregated volume traded during trade day.
+        /// </summary>
+        public double Volume { get; set; }
+
+        /// <summary>
+        ///     Yield for the last trade of the day.
+        /// </summary>
+        public double Yield { get; set; }
+
+        /// <summary>
+        ///     Name of instrument.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+    }
+}

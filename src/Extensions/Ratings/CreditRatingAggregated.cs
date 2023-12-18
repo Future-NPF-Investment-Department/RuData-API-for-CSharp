39ba4572b95344be712f8cd2187d3bd82b634747
kinest22 <nirvana897@gmail.com>
2023-12-18 13:33:10 +0300

    CreditRatingAggregated updated in add to last commit

--- src/Extensions/Ratings/CreditRatingAggregated.cs
+++ src/Extensions/Ratings/CreditRatingAggregated.cs
@@ -25,7 +25,7 @@
         /// <summary>
         ///     Agencies ratings used to 
         /// </summary>
-        public IReadOnlyList<CreditRating>? Ratings { get; internal set; }        
+        public IEnumerable<CreditRating>? Ratings { get; internal set; }        
 
         /// <summary>
         ///     Probability of default (PD) that corresponds to <see cref="AggregatedBig3"/> rating.

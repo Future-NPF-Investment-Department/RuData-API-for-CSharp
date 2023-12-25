a1d45ec0e0fd6ec0954677eff32bf8969bce3dd9
kinest22 <nirvana897@gmail.com>
2023-12-25 09:24:27 +0300

    InstrumentInfoComponent enum added to scope

---
+++ src/Extensions/InstrumentInfoComponent.cs
@@ -0,0 +1,12 @@
+﻿
+namespace RuDataAPI.Extensions
+{
+    [Flags]
+    public enum InstrumentInfoComponent
+    {
+        Core            = 1 << 0,
+        Flows           = 1 << 1,
+        TradeHistory    = 1 << 2,
+        Ratings         = 1 << 3
+    }
+}

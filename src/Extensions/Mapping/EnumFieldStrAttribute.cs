3eb7757a2c7741995f101ad162efd44e1c18bfde
kinest22 <nirvana897@gmail.com>
2023-12-25 09:21:10 +0300

    EnumFieldStrAttribute updated

--- src/Extensions/Mapping/EnumFieldStrAttribute.cs
+++ src/Extensions/Mapping/EnumFieldStrAttribute.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace RuDataAPI.Extensions.Mapping
+﻿namespace RuDataAPI.Extensions.Mapping
 {
     /// <summary>
     ///     Represents collection of alternative string descriptions for enum field.
@@ -8,12 +6,22 @@ namespace RuDataAPI.Extensions.Mapping
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
     public class EnumFieldStrAttribute : Attribute
     {
+        private HashSet<string> _values;
+
+        /// <summary>
+        ///     Creates string-enum mapping with predefined string values to be mapped.
+        /// </summary>        
         public EnumFieldStrAttribute(params string[] strvals) 
-            => Values = new HashSet<string>(strvals);
+            => _values = new HashSet<string>(strvals);
         
         /// <summary>
-        ///     Set of possible unique string values.
+        ///     Values for mapping.
         /// </summary>
-        public HashSet<string> Values { get; set; }
+        public HashSet<string> Values => _values;
+
+        /// <summary>
+        ///     Value used for printing output.
+        /// </summary>
+        public string PrintString { get; init; } = string.Empty;
     }
 }

581baed3127d38da6560b6629f06e0ab495013c8
kinest22 <nirvana897@gmail.com>
2024-04-10 17:38:28 +0300

    InstrumentFlow properties setter changed to public set

--- src/Extensions/InstrumentFlow.cs
+++ src/Extensions/InstrumentFlow.cs
@@ -10,36 +10,36 @@ namespace RuDataAPI.Extensions
         /// <summary>
         ///     Security ISIN-code.
         /// </summary>
-        public string Isin { get; init; } = string.Empty;
+        public string Isin { get; set; } = string.Empty;
 
         /// <summary>
         ///     Event period start date. For CPN events - coupon period start date. For MTY - bond distribution date.
         /// </summary>
-        public DateTime StartDate { get; init; }
+        public DateTime StartDate { get; set; }
 
         /// <summary>
         ///     Event period end date. For CPN events - coupon period end date. For MTY - bond maturity date.
         /// </summary>
-        public DateTime EndDate { get; init; }
+        public DateTime EndDate { get; set; }
         
         /// <summary>
         ///     Length of coupon period (for CPN events) or length of bond lifetime (for MTY events) in days.
         /// </summary>        
-        public int PeriodLength { get; init; }
+        public int PeriodLength { get; set; }
 
         /// <summary>
         ///     Annual interest rate established for the coupon period.
         /// </summary>
-        public double Rate { get; init; }
+        public double Rate { get; set; }
 
         /// <summary>
         ///     Payment per bond for this coupon period in units of bond's notional currency.
         /// </summary>
-        public double Payment { get; init; }
+        public double Payment { get; set; }
 
         /// <summary>
         ///     Payment type (CPN, CALL, MTY, CONV, DIV)
         /// </summary>
-        public FlowType PaymentType { get; init; }
+        public FlowType PaymentType { get; set; }
     }
 }
